da4398c615fd99daa152e8a3a291f8930646fc79
sunil6400 <sunilbhakar6400@gmail.com>
2024-10-17 11:01:42 +0530

    Initial commit

---
+++ src/pages/List.jsx
@@ -0,0 +1,9 @@
+const List = () => {
+  return (
+    <div>
+      <p>List Page works!</p>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,9 @@
+const Orders = () => {
+  return (
+    <div>
+      <p>Orders Page works!</p>
+    </div>
+  );
+};
+
+export default Orders;
